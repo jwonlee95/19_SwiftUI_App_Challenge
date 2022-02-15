dee81b2fff44f520812cfa212af59e1da2aae3c2
jwonlee95 <96804163+jwonlee95@users.noreply.github.com>
2022-02-16 01:49:05 +0900

    2nd Project Done (GuessTheFlag)

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jae Won Lee on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
