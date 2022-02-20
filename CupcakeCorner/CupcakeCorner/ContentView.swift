42d2e361d194d008e909d400b19c6b2a2caa9301
jwonlee95 <96804163+jwonlee95@users.noreply.github.com>
2022-02-21 04:36:57 +0900

    7th Project Done (CupcakeCorner)

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Jae Won Lee on 2022/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var order = Order()
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cupcake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cupcakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery Details")
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
