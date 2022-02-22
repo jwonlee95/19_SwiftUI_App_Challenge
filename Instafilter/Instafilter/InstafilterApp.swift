b76a7aa2c856c782af9311a0d16d5a8c8c675a28
jwonlee95 <96804163+jwonlee95@users.noreply.github.com>
2022-02-23 05:21:01 +0900

    9th Project Done (Instafilter)

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jae Won Lee on 2022/02/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
